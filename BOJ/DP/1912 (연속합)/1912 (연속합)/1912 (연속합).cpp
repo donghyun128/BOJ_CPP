2e9c3b49bfd5b799ba09f1f15f5c941472e9774f
Malz <43366200+donghyun128@users.noreply.github.com>
2021-05-17 22:21:59 +0900

    1966,1912 (unsolved)
    고민했지만 풀지 못했음. 다음을 기약.

---
+++ BOJ/DP/1912 (연속합)/1912 (연속합)/1912 (연속합).cpp
@@ -0,0 +1,59 @@
+﻿// 1912 (연속합).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1002][1002] = {-100000};
+
+int succ_sum(int input,int arr[])
+{
+    int num = input;
+    for (int j = 0; j < input-1; j++)
+    {
+        dp[2][j] = arr[j] + arr[j + 1];
+    }
+
+    for (int i = 3; i < num-1; i++)
+    {
+        for (int j = 0; j < num - i;)
+        {
+            dp[i][j] = dp[i - 1][j] + arr[j + 2];
+        }
+    }
+    int max= -100000;
+    
+    for (int i = 2; i < num - 1; i++)
+    {
+        for (int j = 0; j < num - i; j++)
+        {
+            if (dp[i][j] > max)
+            {
+                max = dp[i][j];
+            }
+        }
+    }
+
+    return max;
+}
+
+
+int main()
+{
+    int input;
+    int arr[100000];
+    cin >> input;
+    
+    for (int i = 0; i < input; i++)
+    {
+        cin >> arr[i];
+    }
+     int result = succ_sum(input, arr);
+     cout << result << endl;
+
+
+     return 0;
+
+}
+
---
+++ BOJ/자료구조/1966(프린터 큐)/1966(프린터 큐)/1966(프린터 큐).cpp
@@ -0,0 +1,45 @@
+﻿// 1966(프린터 큐).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool cmp()
+{}
+
+
+int main()
+{
+    priority_queue<pair<int,int>> pq;
+    int t_num;
+    int d_num, dest;
+    
+
+    cin >> t_num;
+    for (int i = 0; i < t_num; i++)
+    {
+        cin >> d_num >> dest;
+        for (int i = 0; i < d_num; i++)
+        {
+            int tmp;
+            cin >> tmp;
+            pq.push({ tmp,i }); // tmp: 중요도 , i : index
+       }
+        int result = 0;
+        while (!pq.empty())
+        {
+            result++;
+            if (pq.top().second == dest)
+            {
+                cout << result << endl;
+            }
+            pq.pop();
+        }
+
+    }
+    return 0;
+}
+
