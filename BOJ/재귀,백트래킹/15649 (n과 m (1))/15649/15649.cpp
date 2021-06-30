dd22f04b92027d3df584bdbe42f2a9b21eab20f8
Malz <43366200+donghyun128@users.noreply.github.com>
2021-06-30 22:38:51 +0900

    15649 (n and m)

---
+++ BOJ/재귀,백트래킹/15649 (n과 m (1))/15649/15649.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool checked[10] = {0};
+
+void func(int k)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (!checked[i])
+		{
+			checked[i] = 1;
+			arr[k] = i ;
+			func(k + 1);
+			checked[i] = 0;
+		}
+	}
+}
+int main()
+{
+	cin >> n >> m;
+	func(0);
+}
+
