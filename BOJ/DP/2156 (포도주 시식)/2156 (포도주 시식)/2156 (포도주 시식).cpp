b145cf2c58753da3ee12d070fe50e75de4096271
Malz <43366200+donghyun128@users.noreply.github.com>
2021-07-20 21:43:22 +0900

    2156

---
+++ BOJ/DP/2156 (포도주 시식)/2156 (포도주 시식)/2156 (포도주 시식).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int juice[10001] = { 0 };
+int dp[10001] = { 0 };
+
+int cal(int n)
+{
+	int tmp = 0;
+	for (int i = 3; i <= n; i++)
+	{
+		tmp = max(dp[i - 1], dp[i - 3] + juice[i] + juice[i - 1]);
+		tmp = max(tmp, dp[i - 2] + juice[i]);
+		dp[i] = tmp;
+	}
+	return dp[n];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> juice[i];
+	}
+	dp[1] = juice[1];
+	dp[2] = juice[1] + juice[2];
+
+	printf("%d", cal(n));
+
+
+}
