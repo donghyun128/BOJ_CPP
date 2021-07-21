cba45c2a4a8c72ab71c4e67cc02c9820cc4b76cf
Malz <43366200+donghyun128@users.noreply.github.com>
2021-07-21 22:58:10 +0900

    2579

---
+++ BOJ/DP/2579 (계단오르기)/2579 (계단오르기)/2579 (계단오르기).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int score[301] = { 0 };
+int sum[301] = { 0 };
+int cal(int n)
+{
+	for (int i = 3; i<=n; i++)
+	{
+		int tmp;
+		tmp = max(sum[i - 3] + score[i - 1] + score[i], sum[i - 2] + score[i]);
+		sum[i] = tmp;
+	}
+	return sum[n];
+}
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> score[i];
+	}
+	sum[1] = score[1];
+	sum[2] = score[1] + score[2];
+
+	printf("%d", cal(n));
+}
+
