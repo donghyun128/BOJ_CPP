bec02e77adf09b3ff0a53963fefbe265ff6ac3bf
Malz <43366200+donghyun128@users.noreply.github.com>
2021-07-29 21:28:23 +0900

    1654

---
+++ BOJ/이분탐색/랜선 자르기 (1654)/랜선 자르기 (1654)/랜선 자르기 (1654).cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int func(int wlan[], int k,int n , long long maxlan)
+{
+	long long left = 1;
+	long long right = maxlan;
+	long long mid = (left + right) / 2;
+	int answer = 0;
+	int tmp = 0;
+	while (left <= right)
+	{
+		int n_split = 0;
+		mid = (left + right) / 2;
+		for (int i = 1; i <= k; i++)
+		{
+			n_split += wlan[i] / mid;
+		}
+
+		if (n_split >= n)
+		{
+			answer = mid;
+			left = mid + 1;
+		}
+
+		else if (n_split < n)
+		{
+			right = mid - 1;
+		}
+
+
+	
+	}
+	return answer;
+}
+
+
+int main()
+{
+	int k, n;
+	int wlan[10001];
+	long long maxlan = 0;
+	cin >> k >> n;
+	for (int i = 1; i <= k; i++)
+	{
+		cin >> wlan[i];
+		if (maxlan < wlan[i])
+			maxlan = wlan[i];
+	}
+
+	int answer = func(wlan, k, n, maxlan);
+	printf("%d", answer);
+
+}
