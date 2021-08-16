89661e5698e7c60930d1e4be6253e33f79fafdf8
donghyun128 <43366200+donghyun128@users.noreply.github.com>
2021-08-16 22:53:43 +0900

    11057

---
+++ BOJ/DP/11055 (가장 큰 증가 부분 수열)/11055 (가장 큰 증가 부분 수열)/11055 (가장 큰 증가 부분 수열).cpp
@@ -0,0 +1,43 @@
+﻿// 11055 (가장 큰 증가 부분 수열).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[1001];
+int S[1001];
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    arr[0] = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        int sum = 0;
+        int tmp = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                sum = max(sum, S[j] + arr[i]);
+            }
+        }
+        S[i] = sum;
+    }
+
+    int max = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (S[i] > max)
+            max = S[i];
+    }
+    printf("%d", max);
+}
+
+
---
+++ BOJ/DP/11057 (오르막 수)/11057 (오르막 수)/11057 (오르막 수).cpp
@@ -0,0 +1,47 @@
+﻿// 11057 (오르막 수).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#define mod 10007
+using namespace std;
+long long int d[1001][10];
+
+int func(int N)
+{
+    for (int i = 2; i <= N; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            for (int k = 0; k <= j; k++)
+            {
+                d[i][j] += d[i - 1][k];
+                d[i][j] = d[i][j] % mod;
+            }
+        }
+        
+    }
+    
+    int ret = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        ret += d[N][i];
+    }
+
+    return ret % mod;
+}
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 0; i <10; i++)
+    {
+       d[1][i] = 1;
+    }
+
+    printf("%lld", func(N));
+}
