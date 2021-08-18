fbf1e18528eaa680196e4653782ea0c4aeef3eeb
donghyun128 <43366200+donghyun128@users.noreply.github.com>
2021-08-18 22:32:16 +0900

    11727

---
+++ BOJ/DP/11727 (2xn 타일링 2)/11727 (2xn 타일링 2)/11727 (2xn 타일링 2).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+long long int dp[1001];
+
+long long int func(int n)
+{
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] % 10007 + 2 * dp[i - 2] % 10007;
+    }
+
+    return dp[n] % 10007;
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << func(n);
+}
