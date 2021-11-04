68da16bb6e51e17546b7b4361d48a1d29e7564c0
Malz <43366200+donghyun128@users.noreply.github.com>
2021-11-05 00:01:16 +0900

    곱셈

---
+++ BOJ/재귀,백트래킹/1629 (곱셈)/1629 (곱셈)/1629 (곱셈).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+long long int recur_multiply(int n, int count, int mod)
+{
+	if (count == 0)
+		return 1;
+	long long int half;
+	half = recur_multiply(n, count / 2, mod);
+	half = half * half % mod;
+	if (count % 2 == 0)
+		return half;
+	else
+		return half * n % mod;
+}
+
+int main()
+{
+	long long int a, b, c;
+	cin >> a >> b >> c;
+	cout << recur_multiply(a, b, c);
+
+}
