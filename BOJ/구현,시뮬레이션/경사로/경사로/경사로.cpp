0e1cabf5da63e78abbfc98019abcd00e952cfd1e
donghyun128 <43366200+donghyun128@users.noreply.github.com>
2022-03-14 18:46:21 +0900

    a

---
+++ BOJ/구현,시뮬레이션/경사로/경사로/경사로.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int N, L;
+int road[100][100] = { 0 };
+
+
+void input()
+{
+	cin >> N >> L;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> road[i][j];
+		}
+	}
+}
+
+int main()
+{
+}
+
