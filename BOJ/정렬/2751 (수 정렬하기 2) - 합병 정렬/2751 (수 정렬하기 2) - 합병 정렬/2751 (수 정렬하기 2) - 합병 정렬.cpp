53834eb4565237164eeb4094a9bef53a4f4d128b
donghyun128 <donghyun128@gmail.com>
2022-09-15 17:17:28 +0900

    sorting

---
+++ BOJ/정렬/2751 (수 정렬하기 2) - 합병 정렬/2751 (수 정렬하기 2) - 합병 정렬/2751 (수 정렬하기 2) - 합병 정렬.cpp
@@ -0,0 +1,68 @@
+﻿// 2751 (수 정렬하기 2) - 합병 정렬.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int tmpArr[1000000];
+int arr[1000000];
+
+void mergeTwoArea(int arr[],int left,int right,int mid)
+{
+	int lidx = left;
+	int ridx = mid+1;
+	int nidx = left;
+
+	while (lidx <= mid && ridx <= right)
+	{
+		if (arr[lidx] < arr[ridx])
+			tmpArr[nidx] = arr[lidx++];
+		else
+			tmpArr[nidx] = arr[ridx++];
+		nidx++;
+	}
+
+	if (lidx > mid)
+	{
+		for (int i = ridx; i <= right; i++, nidx++)
+			tmpArr[nidx] = arr[i];
+	}
+	else
+	{
+		for (int i = lidx; i <= mid; i++, nidx++)
+			tmpArr[nidx] = arr[i];
+	}
+
+	for (int i = left; i <= right; i++)
+	{
+		arr[i] = tmpArr[i];
+	}
+
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+	int mid;
+
+	if (left < right)
+	{
+		mid = (left + right) / 2;
+		mergeSort(arr, left, mid);
+		mergeSort(arr, mid + 1, right);
+		mergeTwoArea(arr, left, right, mid);
+	}
+
+}
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	mergeSort(arr, 0, N - 1);
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+}
+
---
+++ BOJ/정렬/2751 (수 정렬하기 2)/2751 (수 정렬하기 2)/2751 (수 정렬하기 2).cpp
@@ -0,0 +1,60 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+int arr[1000000];
+
+void swap(int arr[], int idx1, int idx2)
+{
+	int tmp = arr[idx1];
+	arr[idx1] = arr[idx2];
+	arr[idx2] = tmp;
+}
+
+int getRand(int s, int e) {
+	int high = rand();
+	int low = rand();
+	int r = (high << 16) | low;
+
+	return s + (r % (e - s + 1));
+}
+
+void quickSort(int arr[], int left, int right)
+{
+	int pivot = getRand(left, right);
+	int low = left;
+	int high = right;
+
+	while (low <= high)
+	{
+		while (low <= right && arr[low] < arr[pivot])
+			low++;
+		while (high >= left && arr[high] > arr[pivot])
+			high--;
+
+		if (low <= high)
+			swap(arr, low++, high--);
+	}
+
+	if (left < high)
+		quickSort(arr, left, high);
+	if (low < right)
+		quickSort(arr, low, right);
+	
+}
+
+
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	quickSort(arr, 0, N - 1);
+	for (int i = 0; i < N; i++)
+		printf("%d\n", arr[i]);
+}
+
